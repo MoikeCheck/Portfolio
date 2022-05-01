b574337d3263b192f15f8876805f71bd655f4561
MoikeCheck <mikewheatley@hotmail.co.uk>
2022-05-01 19:59:52 +0100

    contact page updated

--- pages/contacts.js
+++ pages/contacts.js
@@ -1,59 +1,188 @@
 import Head from "next/head";
-import { Heading, Stack, Image, Text } from "@chakra-ui/react";
-
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Link,
+  Stack,
+  Textarea,
+  Tooltip,
+  useClipboard,
+  useColorModeValue,
+  VStack,
+} from "@chakra-ui/react";
 import Container from "../components/Container";
+import { BsGithub, BsLinkedin, BsPerson, BsTwitter } from "react-icons/bs";
+import { MdEmail, MdOutlineEmail } from "react-icons/md";
 
 const Contacts = () => {
+  const { hasCopied, onCopy } = useClipboard("mikewheatley@hotmail.co.uk");
+
   return (
     <Container>
       <Head>
         <title>Contacts - Mike Wheatley</title>
       </Head>
-      <Text textAlign={"center"} pt={10}>
-        If you need to contact me please find me details below.
-      </Text>
-      <Stack
-        pt={20}
-        spacing={8}
-        direction="row"
-        justifyContent="center"
-        alignItems="flex-start"
-      >
-        <Stack>
-          <Image
-            ml={12}
-            boxSize="200px"
-            src="/assets/linkedin.png"
-            alt="LinkedIn icon image"
-          />
-          <Heading fontSize="xl" textAlign={"center"}>
-            LinkedIn
-          </Heading>
-        </Stack>
+      <Flex align="center" justify="center" id="contact">
+        <Box
+          borderRadius="lg"
+          m={{ base: 5, md: 16, lg: 10 }}
+          p={{ base: 5, lg: 16 }}
+        >
+          <Box>
+            <VStack spacing={{ base: 4, md: 8, lg: 20 }}>
+              <Heading
+                fontSize={{
+                  base: "4xl",
+                  md: "5xl",
+                }}
+              >
+                Get in Touch
+              </Heading>
 
-        <Stack>
-          <Image
-            ml={12}
-            boxSize="200px"
-            src="/assets/email.png"
-            alt="Email icon image"
-          />
-          <Heading fontSize="xl" textAlign={"center"}>
-            Email
-          </Heading>
-        </Stack>
-        <Stack>
-          <Image
-            ml={12}
-            boxSize="200px"
-            src="/assets/github.png"
-            alt="Github icon image"
-          />
-          <Heading fontSize="xl" textAlign={"center"}>
-            Github
-          </Heading>
-        </Stack>
-      </Stack>
+              <Stack
+                spacing={{ base: 4, md: 8, lg: 20 }}
+                direction={{ base: "column", md: "row" }}
+              >
+                <Stack
+                  align="center"
+                  justify="space-around"
+                  direction={{ base: "row", md: "column" }}
+                >
+                  <Tooltip
+                    label={hasCopied ? "Email Copied!" : "Copy Email"}
+                    closeOnClick={false}
+                    hasArrow
+                  >
+                    <IconButton
+                      aria-label="email"
+                      variant="ghost"
+                      size="lg"
+                      fontSize="3xl"
+                      icon={<MdEmail />}
+                      _hover={{
+                        bg: "blue.500",
+                        color: useColorModeValue("white", "gray.700"),
+                      }}
+                      onClick={onCopy}
+                      isRound
+                    />
+                  </Tooltip>
+
+                  <Link href="#">
+                    <IconButton
+                      aria-label="github"
+                      variant="ghost"
+                      size="lg"
+                      fontSize="3xl"
+                      icon={<BsGithub />}
+                      _hover={{
+                        bg: "blue.500",
+                        color: useColorModeValue("white", "gray.700"),
+                      }}
+                      isRound
+                    />
+                  </Link>
+
+                  <Link href="#">
+                    <IconButton
+                      aria-label="twitter"
+                      variant="ghost"
+                      size="lg"
+                      icon={<BsTwitter size="28px" />}
+                      _hover={{
+                        bg: "blue.500",
+                        color: useColorModeValue("white", "gray.700"),
+                      }}
+                      isRound
+                    />
+                  </Link>
+
+                  <Link href="#">
+                    <IconButton
+                      aria-label="linkedin"
+                      variant="ghost"
+                      size="lg"
+                      icon={<BsLinkedin size="28px" />}
+                      _hover={{
+                        bg: "blue.500",
+                        color: useColorModeValue("white", "gray.700"),
+                      }}
+                      isRound
+                    />
+                  </Link>
+                </Stack>
+
+                <Box
+                  bg={useColorModeValue("white", "gray.700")}
+                  borderRadius="lg"
+                  p={8}
+                  color={useColorModeValue("gray.700", "whiteAlpha.900")}
+                  shadow="base"
+                >
+                  <VStack spacing={5}>
+                    <FormControl isRequired>
+                      <FormLabel>Name</FormLabel>
+
+                      <InputGroup>
+                        <InputLeftElement children={<BsPerson />} />
+                        <Input
+                          type="text"
+                          name="name"
+                          placeholder="Your Name"
+                        />
+                      </InputGroup>
+                    </FormControl>
+
+                    <FormControl isRequired>
+                      <FormLabel>Email</FormLabel>
+
+                      <InputGroup>
+                        <InputLeftElement children={<MdOutlineEmail />} />
+                        <Input
+                          type="email"
+                          name="email"
+                          placeholder="Your Email"
+                        />
+                      </InputGroup>
+                    </FormControl>
+
+                    <FormControl isRequired>
+                      <FormLabel>Message</FormLabel>
+
+                      <Textarea
+                        name="message"
+                        placeholder="Your Message"
+                        rows={6}
+                        resize="none"
+                      />
+                    </FormControl>
+
+                    <Button
+                      colorScheme="blue"
+                      bg="blue.400"
+                      color="white"
+                      _hover={{
+                        bg: "blue.500",
+                      }}
+                      isFullWidth
+                    >
+                      Send Message
+                    </Button>
+                  </VStack>
+                </Box>
+              </Stack>
+            </VStack>
+          </Box>
+        </Box>
+      </Flex>
     </Container>
   );
 };
